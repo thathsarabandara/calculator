4fdccfc82c10db368925fa4785fd4c4b39bace03
thathsarabandara <155525565+thathsarabandara@users.noreply.github.com>
2024-06-15 18:35:18 +0530

    Add javascript code

---
+++ script.js
@@ -0,0 +1,59 @@
+let currentOperand = '';
+let previousOperand = '';
+let operation = null;
+
+function appendNumber(number) {
+    currentOperand += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function compute() {
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result;
+    operation = null;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+    display.innerText = currentOperand;
+}
+
+clearDisplay();
